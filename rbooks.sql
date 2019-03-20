e3f0e2626c8556b619bcb86a77a6d7c83036196f
linjiabao <1285706087@qq.com>
2019-03-20 15:51:50 +0800

    chapter表增加了datetime字段

--- rbooks.sql
+++ rbooks.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50723
  File Encoding         : 65001
 
- Date: 19/03/2019 20:51:08
+ Date: 20/03/2019 15:51:12
 */
 
 SET NAMES utf8mb4;
@@ -32,15 +32,16 @@ CREATE TABLE `book`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `author`(`author`) USING BTREE,
   CONSTRAINT `book_ibfk_1` FOREIGN KEY (`author`) REFERENCES `user` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of book
 -- ----------------------------
-INSERT INTO `book` VALUES (1, '从第一本书改为测试用书', 'krysta', '还是测试', '2019-03-19 08:00:00', 'url', 1111);
+INSERT INTO `book` VALUES (1, '临虚-2 书名也改了', NULL, '这是我很早就想写的小说了，改变了介绍', NULL, NULL, 0);
 INSERT INTO `book` VALUES (6, '第二本书', 'wanwenzhuo', '这是用来测试的', '2019-03-18 22:10:37', '...', 3);
 INSERT INTO `book` VALUES (7, '第三本书', 'krysta', '这还是用来测试的', '2019-03-18 22:12:10', '没有照片', 111);
 INSERT INTO `book` VALUES (8, '第三本书', 'jiangchen', '测试', '2019-03-18 22:12:29', '没有照片', 44);
+INSERT INTO `book` VALUES (13, '临虚-2 书名也改了', NULL, '这是我很早就想写的小说了，改变了介绍', NULL, NULL, 0);
 
 -- ----------------------------
 -- Table structure for chapter
@@ -50,6 +51,7 @@ CREATE TABLE `chapter`  (
   `bookid` int(11) NOT NULL,
   `chaptername` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
   `content` varchar(20000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `datetime` datetime(0) NOT NULL,
   PRIMARY KEY (`bookid`, `chaptername`) USING BTREE,
   INDEX `chaptername`(`chaptername`) USING BTREE,
   CONSTRAINT `chapter_ibfk_1` FOREIGN KEY (`bookid`) REFERENCES `book` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
@@ -58,12 +60,13 @@ CREATE TABLE `chapter`  (
 -- ----------------------------
 -- Records of chapter
 -- ----------------------------
-INSERT INTO `chapter` VALUES (1, '第一章', '该内容');
-INSERT INTO `chapter` VALUES (1, '第二章', '内容更新');
-INSERT INTO `chapter` VALUES (6, '第一章', '第一章内容2');
-INSERT INTO `chapter` VALUES (6, '第二章', '第二章内容2');
-INSERT INTO `chapter` VALUES (7, '第一章', 'bookid 7 第一章');
-INSERT INTO `chapter` VALUES (8, '第一章', 'bookid 8 第一章');
+INSERT INTO `chapter` VALUES (1, '第一章', '该内容', '2019-03-20 15:37:12');
+INSERT INTO `chapter` VALUES (1, '第二章', '内容更新', '2019-03-20 15:37:16');
+INSERT INTO `chapter` VALUES (1, '虚幻职业', '这是增加章节的测试,还更新了内容', '2019-03-20 15:37:20');
+INSERT INTO `chapter` VALUES (6, '第一章', '第一章内容2', '2019-03-20 15:37:25');
+INSERT INTO `chapter` VALUES (6, '第二章', '第二章内容2', '2019-03-20 15:37:29');
+INSERT INTO `chapter` VALUES (7, '第一章', 'bookid 7 第一章', '2019-03-20 15:37:32');
+INSERT INTO `chapter` VALUES (8, '第一章', 'bookid 8 第一章', '2019-03-20 15:37:39');
 
 -- ----------------------------
 -- Table structure for comment
@@ -88,6 +91,7 @@ CREATE TABLE `comment`  (
 -- ----------------------------
 INSERT INTO `comment` VALUES (1, '第一章', '2019-03-19 08:00:00', 'krysta', '这一章写的不错，');
 INSERT INTO `comment` VALUES (1, '第二章', '2019-03-19 09:07:33', 'jiangchen', '这一章不错');
+INSERT INTO `comment` VALUES (1, '虚幻职业', '2018-03-23 08:00:00', 'wanwenzhuo', '这是增加评论,还更新了内容');
 INSERT INTO `comment` VALUES (6, '第一章', '2019-03-19 08:00:00', 'wanwenzhuo', '这一章写的不错，');
 INSERT INTO `comment` VALUES (7, '第一章', '2019-03-19 08:00:00', 'jiangchen', '这一章写的不错，');
 INSERT INTO `comment` VALUES (8, '第一章', '2019-03-19 08:00:00', 'xiahongwei', '这一章写的不错，');
@@ -110,13 +114,11 @@ CREATE TABLE `followauthor`  (
 -- ----------------------------
 INSERT INTO `followauthor` VALUES (3, 0);
 INSERT INTO `followauthor` VALUES (4, 0);
-INSERT INTO `followauthor` VALUES (0, 2);
 INSERT INTO `followauthor` VALUES (3, 2);
 INSERT INTO `followauthor` VALUES (0, 3);
 INSERT INTO `followauthor` VALUES (2, 3);
 INSERT INTO `followauthor` VALUES (0, 4);
 INSERT INTO `followauthor` VALUES (2, 4);
-INSERT INTO `followauthor` VALUES (2, 5);
 
 -- ----------------------------
 -- Table structure for followbook
@@ -134,6 +136,7 @@ CREATE TABLE `followbook`  (
 -- ----------------------------
 -- Records of followbook
 -- ----------------------------
+INSERT INTO `followbook` VALUES (0, 1);
 INSERT INTO `followbook` VALUES (2, 1);
 INSERT INTO `followbook` VALUES (3, 1);
 INSERT INTO `followbook` VALUES (4, 1);
@@ -153,9 +156,9 @@ CREATE TABLE `hibernate_sequence`  (
 -- ----------------------------
 -- Records of hibernate_sequence
 -- ----------------------------
-INSERT INTO `hibernate_sequence` VALUES (12);
-INSERT INTO `hibernate_sequence` VALUES (12);
-INSERT INTO `hibernate_sequence` VALUES (12);
+INSERT INTO `hibernate_sequence` VALUES (14);
+INSERT INTO `hibernate_sequence` VALUES (14);
+INSERT INTO `hibernate_sequence` VALUES (14);
 
 -- ----------------------------
 -- Table structure for information
@@ -163,7 +166,7 @@ INSERT INTO `hibernate_sequence` VALUES (12);
 DROP TABLE IF EXISTS `information`;
 CREATE TABLE `information`  (
   `userid` int(11) NOT NULL,
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `selfintroduction` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `photourl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
@@ -179,7 +182,7 @@ CREATE TABLE `information`  (
 -- Records of information
 -- ----------------------------
 INSERT INTO `information` VALUES (0, 'krysta', 'anti_light@qq.com', 'whuter', '...', 'https://github.com/krystalics', 'krysta.cn');
-INSERT INTO `information` VALUES (2, 'wanwenzhuo', 'xx.com', 'none', '...', '..', 'fff');
+INSERT INTO `information` VALUES (2, NULL, '这一次设置email', NULL, NULL, NULL, NULL);
 INSERT INTO `information` VALUES (3, 'xiahongwei', 'yy@com', 'whuter from 423', '...', 'none', 'none');
 INSERT INTO `information` VALUES (4, 'jiangchen', 'zz@com', 'whuter ', '...', 'none', 'none');
 
@@ -200,9 +203,8 @@ CREATE TABLE `user`  (
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (0, 'krysta', 'rbooks', 2);
-INSERT INTO `user` VALUES (2, 'wanwenzhuo', '123', 0);
+INSERT INTO `user` VALUES (2, 'wanwenzhuo', '567', 0);
 INSERT INTO `user` VALUES (3, 'xiahongwei', '123', 0);
 INSERT INTO `user` VALUES (4, 'jiangchen', '321', 0);
-INSERT INTO `user` VALUES (5, 'test', '111', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
