ea8bccacb64e305a14fa49d1c9336dde6f7a0c72
linjiabao <1285706087@qq.com>
2019-04-01 18:01:54 +0800

    修改了readme

--- backend/src/main/java/com/example/rbooks/backend/controller/SettingsController.java
+++ backend/src/main/java/com/example/rbooks/backend/controller/SettingsController.java
@@ -2,12 +2,16 @@ package com.example.rbooks.backend.controller;
 
 import com.example.rbooks.backend.entity.Information;
 import com.example.rbooks.backend.service.SettingsService;
+import java.io.File;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class SettingsController {
@@ -20,9 +24,12 @@ public class SettingsController {
     this.settingsServiceImpl = settingsServiceImpl;
   }
 
-  @RequestMapping(value = "/settings",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+  @RequestMapping(value = "/settings",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public String update(@RequestBody Information information){ //之后的版本会有需要照片上传，文件的传输之后迭代时再搞
+
     settingsServiceImpl.update(information);
     return "设置成功";
+
+
   }
 }
