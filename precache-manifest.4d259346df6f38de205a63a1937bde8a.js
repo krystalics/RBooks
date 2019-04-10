8d33ae92c81a6c480f91f001a6c909bbc17bb647
linjiabao <1285706087@qq.com>
2019-04-10 10:05:45 +0800

    Updates

---
+++ precache-manifest.4d259346df6f38de205a63a1937bde8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbd924a9862891b98c97",
+    "url": "/frontend/static/js/runtime~main.f332d61a.js"
+  },
+  {
+    "revision": "1562a6eba678538099f2",
+    "url": "/frontend/static/js/main.ad59056a.chunk.js"
+  },
+  {
+    "revision": "d4b54df46bb2ff6436cf",
+    "url": "/frontend/static/js/2.69a97351.chunk.js"
+  },
+  {
+    "revision": "1562a6eba678538099f2",
+    "url": "/frontend/static/css/main.a9307348.chunk.css"
+  },
+  {
+    "revision": "2db72d18cfcc8978caf510d2c81d64a6",
+    "url": "/frontend/index.html"
+  }
+];
